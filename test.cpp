019f11e2a3287511a31cbd2a296367dd781b3982
scrollshadow <zaldhypramana@gmail.com>
2026-05-01 15:50:32 +0700

    7

--- test.cpp
+++ test.cpp
@@ -81,4 +81,19 @@ public:
 
         current->next = newNode;    
     }
+
+    void hapus()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+
+        cout << "\nEnter the roll number of the student whose record is to be delete: ";
+        int rollNo;
+        cin >> rollNo;
+
+        Node *current = START;
+    }
 }
